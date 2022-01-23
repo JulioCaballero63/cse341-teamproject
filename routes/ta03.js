4480d9524e0273fecacfbb595ea0f3804512c3b7
JulioCaballero63 <juliocl_14@hotmail.com>
2022-01-22 23:07:52 -0700

    update

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,14 +1,32 @@
 // //TA03 PLACEHOLDER
+const http = require("https");
 const fs = require('fs');
 const express = require('express');
+const { response } = require("express");
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('pages/ta03', {
-    title: 'Team Activity 03',
-    path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+  const url = 'https://byui-cse.github.io/cse341-course/lesson03/items.json';
+  http.get(url, function (response) {
+    let body = '';
+    response.on('data', function (chunk) {
+      body += chunk;
+    });
+    response.on('end', function () {
+      const jsonResponse = JSON.parse(body);
+      // console.log("Got a response: ", jsonResponse[0].name);
+
+      const data = jsonResponse
+
+      res.render('pages/ta03', {
+        data,
+        title: 'Team Activity 03',
+        path: '/ta03'
+      });
+
+    });
+  }); response.on('error', function (e) {
+    console.log("Got an error: ", e);
   });
 });
 
