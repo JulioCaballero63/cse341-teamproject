b15b09e0d91fc61cf2d4975a231ff9e132282056
JulioCaballero63 <juliocl_14@hotmail.com>
2022-01-22 20:10:23 -0700

    proveit03

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,14 +1,33 @@
-//TA03 PLACEHOLDER
+// //TA03 PLACEHOLDER
+// const fs = require('fs');
+// const express = require('express');
+// const router = express.Router();
+
+// router.get('/', (req, res, next) => {
+//   res.render('pages/ta03', {
+//     title: 'Team Activity 03',
+//     path: '/ta03', // For pug, EJS
+//     activeTA03: true, // For HBS
+//     contentCSS: true, // For HBS
+//   });
+// });
+
+const fs = require('fs');
+const ejs = require('ejs');
+
 const express = require('express');
-const router = express.Router();
+var jsonEngine = require('./jsonEngine.js.js');
+var app = express();
 
-router.get('/', (req, res, next) => {
-  res.render('pages/ta03', {
-    title: 'Team Activity 03',
-    path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+// web server setup
+app
+  .set('port', process.env.PORT || 5000)
+  .use(express.static(__dirname + '/public'))
+  .set('views', __dirname + '/views')
+  .set('view engine', 'ejs')
+  .get('/', jsonEngine.processJson)
+  .listen(app.get('port'), function () {
+    console.log('Listening on port: ' + app.get('port'));
   });
-});
 
-module.exports = router;
+// module.exports = router;
