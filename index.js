c501b66bc30c69e9c5f0897c5ac6ccde447d58cd
JulioCaballero63 <juliocl_14@hotmail.com>
2022-02-05 00:53:31 -0700

    Completed ta05

--- index.js
+++ index.js
@@ -1,43 +1,53 @@
-/*******************************************************************************
- * Feel free to remove this comment block and all other comments after pulling.
- * They're for information purposes only.
- *
- * This layout is provided to you for an easy and quick setup to either pull
- * or use to correct yours after working at least 1 hour on Team Activity 02.
- * Throughout the course, we'll be using Express.js for our view engines.
- * However, feel free to use pug or handlebars ('with extension hbs'). You will
- * need to make sure you install them beforehand according to the reading from
- * Udemy course.
- * IMPORTANT: Make sure to run "npm install" in your root before "npm start"
- *******************************************************************************/
-// Our initial setup (package requires, port number setup)
+const path = require('path');
+// const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const PORT = process.env.PORT || 3000; // So we can run on heroku || (OR) localhost:5000
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const MONGODB_URL = process.env.MONGODB_URL;
 
 const app = express();
 
-// Route setup. You can implement more in the future!
+const store = new MongoDBStore({
+  uri: MONGODB_URL,
+  collection: 'ta05'
+});
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const ta01Routes = require('./routes/ta01');
 const ta02Routes = require('./routes/ta02');
 const ta03Routes = require('./routes/ta03');
 const ta04Routes = require('./routes/ta04');
+const ta05Routes = require('./routes/ta05');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24
+    }
+  })
+)
 
 app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  // For view engine as Pug
-  //.set('view engine', 'pug') // For view engine as PUG.
-  // For view engine as hbs (Handlebars)
-  //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
-  //.set('view engine', 'hbs')
-  .use(bodyParser.urlencoded({ extended: false })) // For parsing the body of a POST
   .use('/ta01', ta01Routes)
   .use('/ta02', ta02Routes)
   .use('/ta03', ta03Routes)
   .use('/ta04', ta04Routes)
+  .use('/ta05', ta05Routes)
+
   .get('/', (req, res, next) => {
     // This is the primary index, always handled last.
     res.render('pages/index', {
@@ -49,4 +59,20 @@ app
     // 404 page
     res.render('pages/404', { title: '404 - Page Not Found', path: req.url });
   })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+mongoose
+  .connect(
+    MONGODB_URL, options
+  )
+  .then(result => {
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
